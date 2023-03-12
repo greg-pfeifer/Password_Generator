541cf7dcfac1605af859ac37da539a11d6b50681
Greg P <gpfeif20@gmail.com>
2023-03-12 16:17:11 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// Assignment Code
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+function clicked() {
+  confirm('Would you like to generate a random password?')
+  {confirm("Ok starting engine now!")};
+  prompt('Number of characters?'); if (prompt <8) {
+    prompt('That is too few characters')
+  } 
+  }
+
+var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener('click', clicked)
+
+
+function numberChars() {
+  prompt('Type the number of characters you would like in your password (must be between 8 and 128')
+}
